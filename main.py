3d4d560b0daa9633e66fe94c2425b9d5dc1cd2a5
ikermartinezcaceres-spec <iker.martinezcaceres@lavegadigital.es>
2025-10-20 20:57:41 +0200

    Add API call for prediction in main.py
    Esta AI te responde con maxima precision y con maxima definicion en los diferentes aparytados de la AI

---
+++ main.py
@@ -0,0 +1,4 @@
+curl https://cloud.flowiseai.com/api/v1/prediction/6ff3cc92-060d-45ef-8857-929ede63e017 \
+     -X POST \
+     -d '{"question": "Hey, how are you?"}' \
+     -H "Content-Type: application/json"
